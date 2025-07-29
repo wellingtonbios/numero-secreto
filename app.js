dbc9e6b6c4538ff99a71fd07353e57b42086dee2
Wellington <wellington.bios@gmail.com>
2025-07-28 21:58:33 -0300

    alterado limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroMaximo = 10;
+let numeroMaximo = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -36,7 +36,7 @@ function gerarNumeroAleatorio(){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1','Jogo do Numero Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 function foco(){
